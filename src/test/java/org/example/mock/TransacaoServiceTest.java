ce542fe1904d210d63746df950a64e7abc8c0047
Willian Takahashi <willian.takahashi@dinfor.uniceub>
2024-10-17 10:00:25 -0300

    feat/mocks

--- src/test/java/org/example/mock/TransacaoServiceTest.java
+++ src/test/java/org/example/mock/TransacaoServiceTest.java
@@ -27,4 +27,61 @@ class TransacaoServiceTest {
         //Assert
         assertFalse(saqueRealizado);
     }
+
+    @Test
+    void realizarSaque_pagamentoAutorizado_returnsTrue() {
+        // Arrange
+        AutorizacaoPagamentoService pagamentoService = Mockito.mock(AutorizacaoPagamentoService.class);
+        when(pagamentoService.autorizarPagamento(anyDouble())).thenReturn(true);
+
+        transacaoService = new TransacaoService(pagamentoService);
+
+        Conta conta = new Conta("1");
+        conta.depositar(10);
+
+        // Act
+        boolean saqueRealizado = transacaoService.realizarSaque(conta, 9);
+
+        // Assert
+        assertTrue(saqueRealizado);
+        assertEquals(1, conta.getSaldo(), 0.01);
+    }
+
+    @Test
+    void realizarDeposito_pagamentoAutorizado_depositoTrue() {
+        // Arrange
+        AutorizacaoPagamentoService pagamentoService = Mockito.mock(AutorizacaoPagamentoService.class);
+        when(pagamentoService.autorizarPagamento(anyDouble())).thenReturn(true);
+
+        transacaoService = new TransacaoService(pagamentoService);
+
+        Conta conta = new Conta("1");
+        conta.depositar(100);
+
+        // Act
+        boolean depositoRealizado = transacaoService.realizarDeposito(conta, 50);
+
+        // Assert
+        assertTrue(depositoRealizado);
+        assertEquals(150, conta.getSaldo(), 0.01);
+    }
+
+    @Test
+    void realizarDeposito_pagamentoNaoAutorizado_depositoFalse() {
+        // Arrange
+        AutorizacaoPagamentoService pagamentoService = Mockito.mock(AutorizacaoPagamentoService.class);
+        when(pagamentoService.autorizarPagamento(anyDouble())).thenReturn(false);
+
+        transacaoService = new TransacaoService(pagamentoService);
+
+        Conta conta = new Conta("1");
+        conta.depositar(100);  // Saldo inicial
+
+        // Act
+        boolean depositoRealizado = transacaoService.realizarDeposito(conta, 50);
+
+        // Assert
+        assertFalse(depositoRealizado);
+        assertEquals(100, conta.getSaldo(), 0.01);
+    }
 }
